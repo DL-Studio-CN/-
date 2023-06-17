34843b0bb471222b0507eb4e2dff5a61018d0e4d
Jie <56182161+XiaoJie-150@users.noreply.github.com>
2023-06-17 10:47:03 +0800

    Increase the task of armor changes
    增加盔甲变化任务

---
+++ src/main/kotlin/ink/ptms/chemdah/core/quest/objective/bukkit/paper/IPlayerArmorChange.kt
@@ -0,0 +1,27 @@
+package ink.ptms.chemdah.core.quest.objective.bukkit.paper
+
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent
+import ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
+
+object IPlayerArmorChange : ObjectiveCountableI<PlayerArmorChangeEvent>() {
+    override val name: String = "armor change"
+    override val event: Class<PlayerArmorChangeEvent> = PlayerArmorChangeEvent::class.java
+
+    init {
+        handler {
+            it.player
+        }
+        addSimpleCondition("chest") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)
+        }
+        addSimpleCondition("feet") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)
+        }
+        addSimpleCondition("head") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)
+        }
+        addSimpleCondition("legs") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)
+        }
+    }
+}
