e7ef8805fe8040a832136cae96ffa624a41680ae
Jie <56182161+XiaoJie-150@users.noreply.github.com>
2023-06-17 10:51:17 +0800

    Update IPlayerArmorChange.kt

--- src/main/kotlin/ink/ptms/chemdah/core/quest/objective/bukkit/paper/IPlayerArmorChange.kt
+++ src/main/kotlin/ink/ptms/chemdah/core/quest/objective/bukkit/paper/IPlayerArmorChange.kt
@@ -1,27 +1,30 @@
-package ink.ptms.chemdah.core.quest.objective.bukkit.paper
+package包 ink.ptms.chemdah.core.quest.objective.bukkit.paper
 
-import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent
-import ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
+import导入 com.destroystokyo.paper.event.player.PlayerArmorChangeEvent
+import导入 ink.ptms.chemdah.core.quest.objective.ObjectiveCountableI
 
-object IPlayerArmorChange : ObjectiveCountableI<PlayerArmorChangeEvent>() {
-    override val name: String = "armor change"
-    override val event: Class<PlayerArmorChangeEvent> = PlayerArmorChangeEvent::class.java
+object对象 IPlayerArmorChange : ObjectiveCountableI<PlayerArmorChangeEvent>() {IPlayerArmorChange : ObjectiveCountableI<PlayerArmorChangeEvent>() {
+    重写 val 名称：String = "armor change"override val name: String = "armor change"
+    覆盖 val 事件：Class<PlayerArmorChangeEvent> = PlayerArmorChangeEvent::class.javaoverride val event: Class<PlayerArmorChangeEvent> = PlayerArmorChangeEvent::class.java
 
-    init {
-        handler {
-            it.player
+    在里面 {init {
+        处理程序{
+            它.player
         }
-        addSimpleCondition("chest") { data, e ->
-            data.toInferItem().isItem(e.newItem!!)
+        addSimpleCondition("chest") { 数据，e ->"chest") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)data.toInferItem().isItem(e.newItem!!)
         }
-        addSimpleCondition("feet") { data, e ->
-            data.toInferItem().isItem(e.newItem!!)
+        addSimpleCondition("feet") { 数据，e ->"feet") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)data.toInferItem().isItem(e.newItem!!)
         }
-        addSimpleCondition("head") { data, e ->
-            data.toInferItem().isItem(e.newItem!!)
+        addSimpleCondition("head") { 数据，e ->"head") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)data.toInferItem().isItem(e.newItem!!)
         }
-        addSimpleCondition("legs") { data, e ->
-            data.toInferItem().isItem(e.newItem!!)
+        addSimpleCondition("legs") { 数据，e ->"legs") { data, e ->
+            data.toInferItem().isItem(e.newItem!!)data.toInferItem().isItem(e.newItem!!)
+        }
+        addSimpleCondition("slot") { data, e ->
+            e.slotType.name == data.toString()
         }
     }
 }
